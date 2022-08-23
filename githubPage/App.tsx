a6a63967ebb7004b9afc5c0e26f1cd77b95748eb
mzeenkala23 <tachyonhd@gmail.com>
2022-08-23 21:41:10 +0100

    commit

--- githubPage/App.tsx
+++ githubPage/App.tsx
@@ -1,169 +1,178 @@
-import React, {useRef, useState} from 'react';
-import BeforeAfterSlider from '../src';
+import React, { useRef, useState } from "react";
+import BeforeAfterSlider from "../src";
 
-import './app.scss';
+import "./app.scss";
 
-const IMG_BASE_PATH = '/react-before-after-slider-component';
+const IMG_BASE_PATH = "/react-before-after-slider-component";
 
 function createImageUrl(url: string): string {
-    return window.location.host.includes('localhost')
-        ? url
-        : `${IMG_BASE_PATH}/${url}`;
+  return window.location.host.includes("localhost")
+    ? url
+    : `${IMG_BASE_PATH}/${url}`;
 }
 
 const DEMONSTRATION_DELAY = 500;
 
 const DEFAULT_DURATION = 800;
 type Animation = {
-    start: number,
-    end: number,
-    duration: number;
-}
+  start: number;
+  end: number;
+  duration: number;
+};
 const START_POSITION = 55;
 const END_POSITION_1 = 35;
-const END_POSITION_2 = 80
+const END_POSITION_2 = 80;
 const ANIMATIONS: Animation[] = [
-    {
-        start: START_POSITION,
-        end: END_POSITION_1,
-        duration: DEFAULT_DURATION,
-    },
-    // pause
-    {
-        start: END_POSITION_1,
-        end: END_POSITION_1,
-        duration: 30,
-    },
-    {
-        start: END_POSITION_1,
-        end: END_POSITION_2,
-        duration: DEFAULT_DURATION,
-    },
+  {
+    start: START_POSITION,
+    end: END_POSITION_1,
+    duration: DEFAULT_DURATION,
+  },
+  // pause
+  {
+    start: END_POSITION_1,
+    end: END_POSITION_1,
+    duration: 30,
+  },
+  {
+    start: END_POSITION_1,
+    end: END_POSITION_2,
+    duration: DEFAULT_DURATION,
+  },
 ];
 
 const DELIMITER_WITH_LOGO_STYLES = {
-    width: '50px',
-    height: '50px',
-    backgroundSize: 'cover',
-    borderRadius: 'none',
-}
+  width: "50px",
+  height: "50px",
+  backgroundSize: "cover",
+  borderRadius: "none",
+};
 
 function timePhaseToCoordinadeDifferenceCoefficient(x: number) {
-    return (Math.sin(x * Math.PI - Math.PI / 2) + 1) / 2;
+  return (Math.sin(x * Math.PI - Math.PI / 2) + 1) / 2;
 }
 
 export default function App() {
-    const [{firstImage, secondImage}] = useState({
-        firstImage: {
-            imageUrl: createImageUrl(`/assets/image1.jpg`),
-            alt: 'Image after'
-        },
-        secondImage: {
-            imageUrl: createImageUrl(`/assets/image2.jpg`),
-            alt: 'Image after'
-        }
-    });
+  const [{ firstImage, secondImage }] = useState({
+    firstImage: {
+      imageUrl: createImageUrl(`/assets/image1.jpg`),
+      alt: "Image after",
+    },
+    secondImage: {
+      imageUrl: createImageUrl(`/assets/image2.jpg`),
+      alt: "Image after",
+    },
+  });
 
-    const [feelsOnlyTheDelimiter, setFeelsOnlyTheDelimiter] = useState<boolean>(false);
-    const toggleFeelsOnlyTheDelimiter = () => setFeelsOnlyTheDelimiter(!feelsOnlyTheDelimiter);
-    const [delimiterPercentPosition, setDelimiterPercentPosition] = useState<number>(START_POSITION);
+  const [feelsOnlyTheDelimiter, setFeelsOnlyTheDelimiter] =
+    useState<boolean>(false);
+  const toggleFeelsOnlyTheDelimiter = () =>
+    setFeelsOnlyTheDelimiter(!feelsOnlyTheDelimiter);
+  const [delimiterPercentPosition, setDelimiterPercentPosition] =
+    useState<number>(START_POSITION);
 
-    /** CustomStyles start */
-    const logoUrl = React.useMemo(() => createImageUrl(`/assets/logo.png`), []);
-    const [delimiterIconStyles, setDelimiterIconStyles] = useState<React.CSSProperties | undefined>();
-    const toggleDelimiterIconStyles = () => setDelimiterIconStyles(
-        !delimiterIconStyles
-            ? {...DELIMITER_WITH_LOGO_STYLES, backgroundImage: `url(${logoUrl})`}
-            : undefined
+  /** CustomStyles start */
+  const logoUrl = React.useMemo(() => createImageUrl(`/assets/logo.png`), []);
+  const [delimiterIconStyles, setDelimiterIconStyles] = useState<
+    React.CSSProperties | undefined
+  >();
+  const toggleDelimiterIconStyles = () =>
+    setDelimiterIconStyles(
+      !delimiterIconStyles
+        ? { ...DELIMITER_WITH_LOGO_STYLES, backgroundImage: `url(${logoUrl})` }
+        : undefined
     );
-    const toggleIconStylesButtonText = [
-        'Now: Delimiter icon',
-        (delimiterIconStyles ? 'with' : 'without'),
-        'custom styles',
-    ].join(' ');
-    /** CustomStyles end */
+  const toggleIconStylesButtonText = [
+    "Now: Delimiter icon",
+    delimiterIconStyles ? "with" : "without",
+    "custom styles",
+  ].join(" ");
+  /** CustomStyles end */
 
-    /** Animation start */
-    const allAnimationsRef = useRef<Animation[]>([]);
-    const animationStartTimeRef = useRef<number | null>(null);
-    const animationPositionsRef = useRef<Animation | null>(null);
-    const animate = (timestamp: number) => {
-        let animationPositions = animationPositionsRef.current;
+  /** Animation start */
+  const allAnimationsRef = useRef<Animation[]>([]);
+  const animationStartTimeRef = useRef<number | null>(null);
+  const animationPositionsRef = useRef<Animation | null>(null);
+  const animate = (timestamp: number) => {
+    let animationPositions = animationPositionsRef.current;
 
-        if (!animationPositions) {
-            const currentAnimation = allAnimationsRef.current.shift();
-            if (!currentAnimation) {
-                return;
-            }
-            animationPositions = animationPositionsRef.current = currentAnimation;
-        }
-
-        let animationStartTime = animationStartTimeRef.current;
-        if (!animationStartTime) {
-            animationStartTime = animationStartTimeRef.current = timestamp;
-        }
-
-        const {
-            start: animationStartPosition,
-            end: animationEndPosition,
-            duration: animationDuration
-        } = animationPositions;
-
-        if (timestamp > animationStartTime + animationDuration) {
-            // End of animation
-            setDelimiterPercentPosition(animationEndPosition);
-            animationPositionsRef.current = null;
-            animationStartTimeRef.current = null;
-        } else {
-            const animationPhase = (timestamp - animationStartTime) / animationDuration;
-            const coordinatesDifference =
-                (animationEndPosition - animationStartPosition)
-                * timePhaseToCoordinadeDifferenceCoefficient(animationPhase);
-
-            setDelimiterPercentPosition(animationStartPosition + coordinatesDifference);
-        }
-
-
-        window.requestAnimationFrame(animate);
+    if (!animationPositions) {
+      const currentAnimation = allAnimationsRef.current.shift();
+      if (!currentAnimation) {
+        return;
+      }
+      animationPositions = animationPositionsRef.current = currentAnimation;
     }
 
-    const demonstrate = () => {
-        allAnimationsRef.current = [...ANIMATIONS];
-        setTimeout(() => window.requestAnimationFrame(animate), DEMONSTRATION_DELAY);
-    };
-    /** Animation end */
+    let animationStartTime = animationStartTimeRef.current;
+    if (!animationStartTime) {
+      animationStartTime = animationStartTimeRef.current = timestamp;
+    }
 
-    const buttonText = [
-        'Now:',
-        (feelsOnlyTheDelimiter ? 'Only separator' : 'All area'),
-        'is clickable'
-    ].join(' ');
+    const {
+      start: animationStartPosition,
+      end: animationEndPosition,
+      duration: animationDuration,
+    } = animationPositions;
 
-    return (
-        <div className="app">
-            <div className="app__content-wrapper">
-                <BeforeAfterSlider
-                    currentPercentPosition={delimiterPercentPosition}
-                    firstImage={firstImage}
-                    secondImage={secondImage}
-                    onVisible={demonstrate}
-                    onChangePercentPosition={setDelimiterPercentPosition}
-                    feelsOnlyTheDelimiter={feelsOnlyTheDelimiter}
-                    delimiterIconStyles={delimiterIconStyles}
-                />
-                <div className="app__buttons">
-                    <div className="app__buttons__button-wrapper">
-                        <button onClick={toggleFeelsOnlyTheDelimiter}>
-                             {buttonText}
-                        </button>
-                    </div>
-                    <div className="app__buttons__button-wrapper">
-                        <button onClick={toggleDelimiterIconStyles}>
-                            {toggleIconStylesButtonText}
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+    if (timestamp > animationStartTime + animationDuration) {
+      // End of animation
+      setDelimiterPercentPosition(animationEndPosition);
+      animationPositionsRef.current = null;
+      animationStartTimeRef.current = null;
+    } else {
+      const animationPhase =
+        (timestamp - animationStartTime) / animationDuration;
+      const coordinatesDifference =
+        (animationEndPosition - animationStartPosition) *
+        timePhaseToCoordinadeDifferenceCoefficient(animationPhase);
+
+      setDelimiterPercentPosition(
+        animationStartPosition + coordinatesDifference
+      );
+    }
+
+    window.requestAnimationFrame(animate);
+  };
+
+  const demonstrate = () => {
+    allAnimationsRef.current = [...ANIMATIONS];
+    setTimeout(
+      () => window.requestAnimationFrame(animate),
+      DEMONSTRATION_DELAY
     );
+  };
+  /** Animation end */
+  console.log("ds");
+  const buttonText = [
+    "Now:",
+    feelsOnlyTheDelimiter ? "Only separator" : "All area",
+    "is clickable",
+  ].join(" ");
+
+  return (
+    <div className="app">
+      <div className="app__content-wrapper">
+        <BeforeAfterSlider
+          currentPercentPosition={delimiterPercentPosition}
+          firstImage={firstImage}
+          secondImage={secondImage}
+          onVisible={demonstrate}
+          onChangePercentPosition={setDelimiterPercentPosition}
+          feelsOnlyTheDelimiter={feelsOnlyTheDelimiter}
+          delimiterIconStyles={delimiterIconStyles}
+        />
+        <div className="app__buttons">
+          <div className="app__buttons__button-wrapper">
+            <button onClick={toggleFeelsOnlyTheDelimiter}>{buttonText}</button>
+          </div>
+          <div className="app__buttons__button-wrapper">
+            <button onClick={toggleDelimiterIconStyles}>
+              {toggleIconStylesButtonText}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
